8be239bed487ddb5c8b5858f477c9b7f99a5009c
dcruzaltimetrik <dcruz@altimetrik.com>
2022-02-15 10:36:13 -0300

    [WIP] #19 - Rails Model Exercise

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
+  
+  TOPIC_OPTIONS = %w(others movies gaming books sports)
+  # previous_version_date :datetime
+  enum topic: TOPIC_OPTIONS
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+  validates :topic, inclusion: { in: TOPIC_OPTIONS }
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_032648) do
+ActiveRecord::Schema.define(version: 2022_02_14_060508) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "topic", default: 0
   end
 
   create_table "comments", force: :cascade do |t|
